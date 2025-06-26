c03119beb67990385a18b92ca4c2f78425841717
Natthakit Jantawong <natthakit.ja@ku.th>
2025-06-26 15:33:07 +0700

    update helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,3 +3,10 @@
 // lowerCamelCase -> methodName, variableName, attributeName
 // snake_case -> หักคะแนน
 // abbr -> หักคะแนน
+
+public class HelloWorld {
+    public static void main(string[] args){
+        System.out.println("Hello, World! 2025");
+        
+    }
+}
